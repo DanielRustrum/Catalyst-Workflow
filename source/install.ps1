dc34e68a98ec42a5f0790b39b840c914c0f5cea5
DanielRustrum <danieltrustrum@gmail.com>
2023-04-27 16:52:01 -0700

    Piviot to usering Docker as Dev Environment

---
+++ source/install.ps1
@@ -0,0 +1,18 @@
+#* Run if Admin Access Aquired
+function RunIfAdmin() {
+	#* Fetch User Input For Setup
+	#* Check if Docker is Installed
+		#* Fetch Latest Docker Installer for Windows and Install
+		#* Setup Docker For Catalyst Use 
+	#* Check if Hyper-V/ is Installed
+		#* Setup Hyper-V For Catalyst Use
+		#* Prepare For Virtualization Use
+	#* Check if Desired PowerToys are Installed
+		#* Install and Setup PowerToys
+	#* Setup for Command Use
+}
+
+#* Check if in Admin and Execute Run Function
+If() {
+	RunIfAdmin
+}
--- source/ops/build.ps1
+++
@@ -1,9 +0,0 @@
-function Run() {
-    Invoke-PS2EXE ./Source/core/install.ps1 ./release/install.exe
-}
-
-if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
-    Run
-    Exit
-}
