165f401e2a79db0190a53a09954d6ec0b7d447a0
DanielRustrum <danieltrustrum@gmail.com>
2023-04-27 17:26:24 -0700

    Check for Admin

--- source/install.ps1
+++ source/install.ps1
@@ -1,18 +1,30 @@
 #* Run if Admin Access Aquired
 function RunIfAdmin() {
-	#* Fetch User Input For Setup
+	#* Initial Catalyst Setup
+	#* Inquire User About Configuration
 	#* Check if Docker is Installed
 		#* Fetch Latest Docker Installer for Windows and Install
-		#* Setup Docker For Catalyst Use 
+		#* Setup Docker For Catalyst Use
+	#* Check if Git is Installed
+		#* Fetch Git Installer For Windows and Install
+		#* Setup Git For Catalyst Use
 	#* Check if Hyper-V/ is Installed
 		#* Setup Hyper-V For Catalyst Use
 		#* Prepare For Virtualization Use
 	#* Check if Desired PowerToys are Installed
 		#* Install and Setup PowerToys
-	#* Setup for Command Use
+	#* Final Catalyst Setup
 }
 
 #* Check if in Admin and Execute Run Function
-If() {
-	RunIfAdmin
+if(
+	!(
+		[Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+	).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
+) {
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList \
+    "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
+    
+    RunIfAdmin
+    Exit
 }
